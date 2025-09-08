f1af4a376c212ac643c08427ea6d5d1aeb8afa88
Kalyan Sriram <kalyan@coderkalyan.com>
2025-09-07 20:48:38 -0700

    lab0: update harness

--- lab0/harness.c
+++ lab0/harness.c
@@ -1,14 +1,14 @@
-#include <stdint.h>
+#include <inttypes.h>
 #include <stdio.h>
 
 extern uint32_t umul(const uint32_t x, const uint32_t y);
 
 int main(void) {
     uint32_t x, y;
-    scanf("%lu %lu", &x, &y);
+    scanf("%" PRIu32 " %" PRIu32 "", &x, &y);
 
     const uint32_t result = umul(x, y);
-    printf("%lu\n", result);
+    printf("%" PRIu32 "\n", result);
 
     return 0;
 }
