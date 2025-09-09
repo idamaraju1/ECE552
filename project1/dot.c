e974ff1c639cc77e69a9c2ea701809770e50b1d4
Kalyan Sriram <kgsriram@wisc.edu>
2025-09-09 00:49:36 -0700

    project1: add problem 2

---
+++ project1/dot.c
@@ -0,0 +1,13 @@
+#include <inttypes.h>
+#include <stdio.h>
+
+extern uint32_t umul(const uint32_t x, const uint32_t y);
+
+// Performs a vector dot product between the 32 bit unsigned integer
+// arrays A and B of length len. The arrays are guaranteed to be the
+// same length (len items).
+uint32_t dot(const uint32_t *const A, const uint32_t *const B, const size_t len)
+{
+    // Replace with your implementation of dot product.
+    return 0;
+}
