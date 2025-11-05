648f94ce4d75a0c0157e5cee9764b5e8381f8bd3
Davis Voeller <djvoeller@wisc.edu>
2025-11-05 16:59:37 -0600

    correct pc implementation

--- project5/rtl/pc.v
+++ project5/rtl/pc.v
@@ -4,13 +4,14 @@ module pc #(
 )(
     input  wire        i_clk,
     input  wire        i_rst,       // synchronous active-high reset (per dff.v)
+    input  wire        i_write,
     input  wire [31:0] i_next_pc,
     output reg  [31:0] o_pc
 );
     always @(posedge i_clk) begin
         if (i_rst) begin
             o_pc <= RESET_ADDR;
-        end else begin
+        end else if (i_write) begin
             o_pc <= i_next_pc;
         end
     end
