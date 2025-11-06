9a0a546a32ed52445ef241749f93bcc03efe8ecb
davisjvoeller <121730760+davisjvoeller@users.noreply.github.com>
2025-11-05 20:57:36 -0600

    updated for branch resolution
    Added comments to instructions for clarity.

--- project5/tests/asm/s1test.asm
+++ project5/tests/asm/s1test.asm
@@ -1,8 +1,8 @@
-L0:  addi x7, x0, 0xf
-L1:  addi x1, x0, 10
+L0:  addi x7, x0, 0xf F D
+L1:  addi x1, x0, 10    F D X M W
      nop
      nop
-L2:  addi x2, x1, 1
+L2:  addi x2, x1, 1           F D
      nop 
      nop
 L3:  addi x3, x2, 1
@@ -18,7 +18,9 @@ L6:  addi x6, x5, 1
      nop
      nop
 L7:  beq  x6, x7, L10
-L8:  lui  a0, 0xdead
+     nop                
+     nop                    
+L8:  lui  a0, 0xdead         
 L9:  ebreak
 L10: lui  a0, 0x1
 L11: ebreak
